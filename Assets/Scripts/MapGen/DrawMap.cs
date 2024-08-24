06219dcc54d03aa57ff175dec40831927d549576
Dusan-Stojkovic <dusan.stojkovic@protonmail.com>
2024-08-24 14:33:17 +0200

    Improvements
    Added image example to README;
    Added LICENCE file;
    Exposed width, height and seed through the editor;
    Renamed example scene;

--- Assets/Scripts/MapGen/DrawMap.cs
+++ Assets/Scripts/MapGen/DrawMap.cs
@@ -6,25 +6,29 @@ namespace MapGen
    {
      Map _map;
      Texture2D _texture;
+     [Header("Map parameters")]
      [SerializeField]
-     int _textureWidth = 256;
+     byte seed = 0;
      [SerializeField]
-     int _textureHeight = 256;
+     int textureWidth = 256;
      [SerializeField]
-     Material _dstMaterial;
+     int textureHeight = 256;
+     [SerializeField]
+     [Header("Scene")]
+     Material dstMaterial;
      void Start()
      {
        CreateNewTexture();
-       Map m = new Map(_texture, 600, 600);
+       Map m = new Map(_texture, textureWidth, textureHeight);
      }
      void CreateNewTexture ()
      {
-	    _texture = new Texture2D( width:_textureWidth , height:_textureHeight , textureFormat:TextureFormat.ARGB32 , mipCount:3 , linear:true );
+	    _texture = new Texture2D( width:textureWidth , height:textureHeight , textureFormat:TextureFormat.ARGB32 , mipCount:3 , linear:true );
 	 	_texture.filterMode = FilterMode.Point;// no smooth pixels
 	 	_texture.SetPixel( 0 , 0 , Color.black );
 	 	_texture.SetPixel( x:_texture.width-1 , y:_texture.height-1 , Color.magenta );
 	 	_texture.Apply();
-	 	if( _dstMaterial!=null ) _dstMaterial.mainTexture = _texture;
+	 	if( dstMaterial!=null ) dstMaterial.mainTexture = _texture;
         Debug.Log( "Texture created" );
 	 }
      void Dispose () => Dispose( _texture );
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# LilCubeMapGen
+# Lil2DMapGen
 
 ## Description
-This basic algorithm has an aim to place as many cubes on the surface of another cube.
+This basic algorithm has an aim to place as many rectangle on the surface of another rectangle.
+The following is an example for texture width 512 and height 512 seed is 69:
+
+![alt text](https://github.com/Dusan-Stojkovic/Lil2DMapGen/blob/main/Assets/Resources/Examples/MapGen2.png?raw=true)
 
